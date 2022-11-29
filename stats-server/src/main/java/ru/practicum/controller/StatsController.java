68436aebbf71cc8236d05d18bb472a332b279ed2
Dmitry Smirnov <basilisk@yandex.ru>
2022-11-29 11:48:24 +0000

    fix: исправить лог stats-server

--- stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -26,15 +26,16 @@ public class StatsController {
 
     @PostMapping(path = "/hit")
     void postEndpointHit(@RequestBody @Valid EndpointHitDto endpointHitDto) {
+        log.info("post hit : " + endpointHitDto.toString());
         statsService.postEndpointHit(endpointHitDto);
     }
 
-    @GetMapping(path = "stats")
+    @GetMapping(path = "/stats")
     List<ViewStats> getViewStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
                                  @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                  @RequestParam(required = false) List<String> uris,
                                  @RequestParam(required = false) Boolean unique) {
-        log.info("hit : " + start + " " + end + " " + uris + " " + unique);
+        log.info("get stats : " + start + " " + end + " " + uris + " " + unique);
         return statsService.getViewStats(start, end, uris, unique);
     }
 
